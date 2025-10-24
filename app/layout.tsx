184b131e9b5608506f8998660d13e7fc8e5338ab
sagarmeravi <130913718+sagarmeravi@users.noreply.github.com>
2025-10-24 20:12:09 +0530

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Project Portfolio',
-}
+  title: "Project Portfolio",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Geist+Mono:wght@100..900&display=swap"
           rel="stylesheet"
@@ -29,5 +33,5 @@ html { font-family: var(--font-sans); }
       </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
